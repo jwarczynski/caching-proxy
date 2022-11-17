7264edb7aa2a085f456d5f7e4214543010ee8b16
Jędrzej Warczyński <jedrasowicz@wp.pl>
2022-11-17 07:17:55 +0100

    dostosowanie funkjci processRequest w pliku main

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,16 @@ int main(){
 }
 
 string processRequest(string requestBody) {
-    // Print "Hello, world!" example page
-    return "HTTP/1.1 200 OK\r\n"
-        "Content-Length: 58\r\n"
-        "Content-Type: text/html\r\n"
-        "Connection: Closed\r\n"
-        "\r\n"
-        "<html>\r\n"
-        "<body>\r\n"
-        "<h1>Hello, World!</h1>\r\n"
-        "</body>\r\n"
-        "</html>\r\n";
+
+    string remoteResponse = makeRequest(requestBody);
+    return remoteResponse;
+
 
     string cachedResponse = retrieveFromCache(requestBody);
     if(cachedResponse != "") {
         return cachedResponse;
     }
 
-    string remoteResponse = makeRequest("http://example.com", requestBody);
     saveToCache(requestBody, remoteResponse);
     return remoteResponse;
 }
