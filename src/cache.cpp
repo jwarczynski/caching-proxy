a4599aafff89170d7a653c544773a0ae335a110e
whpac <marcinszwarc@hotmail.com>
2022-11-21 21:22:35 +0100

    Implementacja cache

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,9 +1,49 @@
 #include "cache.hpp"
+#include "http.hpp"
+#include<map>
+#include<iostream>
+
+struct CacheEntryKey {
+    string url;
+};
+
+// Compare the keys, for example lexicographically
+bool operator<(const CacheEntryKey& l, const CacheEntryKey& r) {
+    return (l.url < r.url);
+}
+
+// Value = response body
+map<CacheEntryKey, string> cache;
 
 string retrieveFromCache(string requestBody){
-    return "";
+    string responseBody = "";
+    Request *request = parseRequestBody(requestBody);
+
+    string url = buildUrl(request);
+    CacheEntryKey key = {
+        .url = url
+    };
+    if(cache.find(key) != cache.end()){
+        responseBody = cache.at(key);
+        cout << "Cache hit for: " << url << endl;
+    }else{
+        cout << "Cache miss for: " << url << endl;
+    }
+
+    freeRequest(request);
+    return responseBody;
 }
 
 void saveToCache(string requestBody, string responseBody){
-    
+    Request *request = parseRequestBody(requestBody);
+
+    string url = buildUrl(request);
+    CacheEntryKey key = {
+        .url = url
+    };
+    cache.insert(pair<CacheEntryKey, string>(key, responseBody));
+
+    cout << "Saved to cache: " << url << endl;
+
+    freeRequest(request);
 }
